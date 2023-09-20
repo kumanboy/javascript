8d8af54259e11f790fe1b497c9c34a063884acc6
kumanboy <husandavronov13@gmail.com>
2023-09-20 08:34:49 +0500

    date

---
+++ js2/date.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//Date  hozirgi vaqtni korsak boladi
+
+const now = new Date()
+
+console.log(now)
+
+//Date methodlari getFullYear, getMonth, getDate, getHours,getMinutes, setFullYear, setMonth, setDate ishlatib ko'rish
+
+
+//Loop necha sekund ishlashini Date orqali tekshiramiz
+
+const start = new Date()
+
+for (let i = 0; i < 10000000; i++) {
+    let some = i ** 3
+}
+
+const end = new Date()
+
+alert(`Loop ishladi ${end - start} millisekund`)
